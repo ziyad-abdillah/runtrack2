27de250c31bfcf3acac13b23d7935c15d61ccda6
ziyad-abdillah <ziyad.abdillah@laplateforme.io>
2022-11-14 17:15:42 +0100

    job fini

---
+++ Jour01/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+
+echo $str2 ." ". $str ." ". $str3;
+
+$val = 6;
+echo $val;
+$val = $val + 4;
+echo $val;
+
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
+?>
---
+++ Jour01/job03/index.php
@@ -0,0 +1,13 @@
+<?php 
+$boolean = false;
+$entier = 5;
+$caractere = "Hello";
+$nb_virguele = 5.2;
+
+$array = array( $boolean ,
+$entier ,
+$caractere ,
+$nb_virguele,);
+
+var_dump($array);
+?>
